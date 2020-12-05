9d415cac48fc464a3a5dd54f0a7319500753a56e
leonleon123 <lele.slav@gmail.com>
2020-12-05 13:22:59 +0100

    small fix day 2

--- 04/main.py
+++ 04/main.py
@@ -6,8 +6,8 @@ with open("input.txt") as file:
         "byr": lambda a: a.isdigit() and int(a) >= 1920 and int(a) <= 2002, 
         "iyr": lambda a: a.isdigit() and int(a) >= 2010 and int(a) <= 2020, 
         "eyr": lambda a: a.isdigit() and int(a) >= 2020 and int(a) <= 2030, 
-        "hgt": lambda a: a[:-2].isdigit() and int(a[:-2]) >= (150 if a[-2:] == "cm" else 59) and int(a[:-2]) <= (193 if a[-2:] == "cm" else 76), 
-        "hcl": lambda a: a[0] == "#" and len(re.match("([0-9]|[a-z])*", a[1:]).group()) == 6, 
+        "hgt": lambda a: re.match("[0-9]*(cm|in)", a) and int(a[:-2]) >= (150 if a[-2:] == "cm" else 59) and int(a[:-2]) <= (193 if a[-2:] == "cm" else 76), 
+        "hcl": lambda a: a[0] == "#" and len(re.match("([0-9]|[a-f])*", a[1:]).group()) == 6, 
         "ecl": lambda a: a in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"], 
         "pid": lambda a: len(re.match("[0-9]*", a).group()) == 9
     }
