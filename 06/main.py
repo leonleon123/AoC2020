3b01b82757dee5dea95f5c6a729c67819094479c
leonleon123 <lele.slav@gmail.com>
2020-12-06 11:09:09 +0100

    day 6

--- 06/main.py
+++ 06/main.py
@@ -3,4 +3,4 @@ from functools import reduce
 with open("input.txt") as file:
     t = file.read().split("\n\n")
     print(sum(len(set(b.replace("\n", ""))) for b in t))
-    print(sum(len(reduce(lambda a, acc: a.intersection(acc),  map(set, b.split("\n")))) for b in t))
+    print(sum(len(reduce(lambda a, acc: a & acc,  map(set, b.split("\n")))) for b in t))
