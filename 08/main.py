d429f840fe4482cebc242e68f145af33b86ae171
leonleon123 <lele.slav@gmail.com>
2020-12-08 16:45:14 +0100

    shortened day 8

--- 08/main.py
+++ 08/main.py
@@ -1,22 +1,14 @@
 def run(data):
     pc, acc, ex = 0, 0, set()
     while pc not in ex:
-        if pc < 0 or pc >= len(data): return pc, acc
+        if pc < 0 or pc >= len(data): break
         ex.add(pc)
         op, ar = data[pc]
-        acc += (op == "acc") * ar
-        pc += (ar - 1) * (op == "jmp") + 1
+        acc, pc = acc + (op == "acc") * ar, pc +  (ar - 1) * (op == "jmp") + 1
     return pc, acc
         
 with open("input.txt") as file:
-    data = [(line.split(" ")[0], int(line.split(" ")[1])) for line in file.readlines()]
-    print(run(data)[1])
-    max_pc, max_acc = 0, 0
-    for i in range(len(data)):
-        op, ar = data[i]
-        if op == "jmp" or op == "nop":
-            tmp = [*data]
-            tmp[i] = ("jmp" if op == "nop" else "nop", ar)
-            pc, acc = run(tmp)
-            if pc > max_pc: max_pc, max_acc = pc, acc
-    print(max_acc)
+    d = [(line.split(" ")[0], int(line.split(" ")[1])) for line in file.readlines()]
+    print(run(d)[1])
+    swapped = [run(d[:i] + [(["jmp", "nop"][d[i][0] == "jmp"], d[i][1])] + d[i+1:]) for i in range(len(d)) if d[i][0] in ["jmp", "nop"]]
+    print(max(swapped, key=lambda e: e[0])[1])
